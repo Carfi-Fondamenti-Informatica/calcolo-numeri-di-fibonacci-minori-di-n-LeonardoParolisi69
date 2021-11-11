1243a899de4a0e430ddc1566cfcf371ccbe704af
ProgrammatoreSenzaSperanza <91635848+LeonardoParolisi69@users.noreply.github.com>
2021-11-11 21:55:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-   return 0;
+
+    int n=0, primo=1, secondo=1, fibonacci=0;
+
+    cin>> n;
+    if (n<=0){
+        return 0;
+    }else{
+
+        cout<< "1" << "\n" << "1" << endl;
+
+        for (int i = 2; i > 0; ++i) {
+            fibonacci=primo+secondo;
+            primo=secondo;
+            secondo=fibonacci;
+            if(fibonacci<=n){
+                cout<< fibonacci <<endl;
+            }else{
+                break;
+            }
+        }
+
+    }
+
+    return 0;
 }
